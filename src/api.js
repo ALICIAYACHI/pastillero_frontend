0feef73d0eb66b6d1ea56be062938c83b684559c
Arom Sanchez <arom.sanchez@tecsup.edu.pe>
2026-05-28 14:57:26 -0500

    fix: Update API base URL to correct backend endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://pastillero.onrender.com/api/",
+  baseURL: "https://pastillero-backend-ydbs.onrender.com/api/",
   headers: {
     'Content-Type': 'application/json',
   },
